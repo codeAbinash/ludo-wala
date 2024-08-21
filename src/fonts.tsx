da40e5e0c8beea4aac623c56737ecd7f58dadc41
Abinash Karmakar <codeAbinash@gmail.com>
2024-08-21 16:11:35 +0530

    Add fonts and prettier config

---
+++ src/fonts.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import {Text, type TextProps} from 'react-native'
+
+export const RegularS = {
+  fontFamily: 'Gellix-Regular',
+}
+export const MediumS = {
+  fontFamily: 'Gellix-Medium',
+}
+export const SemiBoldS = {
+  fontFamily: 'Gellix-SemiBold',
+}
+export const BoldS = {
+  fontFamily: 'Gellix-Bold',
+}
+export const LightS = {
+  fontFamily: 'Gellix-Light',
+}
+
+export function Medium({children, style, ...props}: TextProps) {
+  return (
+    <Text style={[MediumS, style]} {...props}>
+      {children}
+    </Text>
+  )
+}
+
+export function Regular({children, style, ...props}: TextProps) {
+  return (
+    <Text style={[RegularS, style]} {...props}>
+      {children}
+    </Text>
+  )
+}
+
+export function SemiBold({children, style, ...props}: TextProps) {
+  return (
+    <Text style={[SemiBoldS, style]} {...props}>
+      {children}
+    </Text>
+  )
+}
+
+export function Bold({children, style, ...props}: TextProps) {
+  return (
+    <Text style={[BoldS, style]} {...props}>
+      {children}
+    </Text>
+  )
+}
+
+export function Light({children, style, ...props}: TextProps) {
+  return (
+    <Text style={[LightS, style]} {...props}>
+      {children}
+    </Text>
+  )
+}
