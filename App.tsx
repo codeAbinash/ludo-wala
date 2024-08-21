53e4dd0b7052fb540e59f4cd858b6f7ce5f8277e
Abinash Karmakar <codeAbinash@gmail.com>
2024-08-21 16:55:06 +0530

    Add support for svg files

--- App.tsx
+++ App.tsx
@@ -5,13 +5,19 @@
  * @format
  */
 
-import {View} from 'react-native'
+import {StatusBar, View} from 'react-native'
 import {Medium} from './src/fonts'
+import LottieView from 'lottie-react-native'
+import {Home01Icon} from './src/assets/icons/icons'
+import React from 'react'
 
 function App(): React.JSX.Element {
   return (
-    <View>
-      <Medium>Hello</Medium>
+    <View className='flex-1 bg-black'>
+      <StatusBar barStyle='light-content' backgroundColor={'transparent'} />
+      <Medium className='mt-10 text-4xl text-white'>Hello</Medium>
+      <LottieView source={require('./src/assets/animations/earth.lottie')} autoPlay loop style={{width: 200, height: 200}} />
+      <Home01Icon className='h-10 w-10 text-white' />
     </View>
   )
 }
---
+++ src/assets/icons/icons.ts
@@ -0,0 +1,1 @@
+export {default as Home01Icon} from '@icons/home-01-stroke-rounded.svg'
