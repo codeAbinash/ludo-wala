14d2de24d8a48cfc5e69dd5bbbc2441860a91c0c
Abinash Karmakar <codeAbinash@gmail.com>
2024-08-24 12:34:54 +0530

    chore: Refactor EditProfile screen component

--- src/screens/EditProfile.tsx
+++ src/screens/EditProfile.tsx
@@ -1,15 +1,16 @@
-import { Medium, SemiBold, SemiBoldS } from '@/fonts'
-import { userStore } from '@/zustand/userStore'
-import { PaintBrush01SolidIcon } from '@assets/icons/icons'
+import {Medium, SemiBold, SemiBoldS} from '@/fonts'
+import {userStore} from '@/zustand/userStore'
+import {PaintBrush01SolidIcon} from '@assets/icons/icons'
 import BackHeader from '@components/BackHeader'
-import { FullGradientButton, LoadingButton } from '@components/Button'
-import { PaddingBottom } from '@components/SafePadding'
+import {FullGradientButton, LoadingButton} from '@components/Button'
+import KeyboardAvoidingContainer from '@components/KeyboardAvoidingContainer'
+import {PaddingBottom} from '@components/SafePadding'
 import Screen from '@components/Screen'
-import { get_user_f, updateProfile_f, type UpdateProfileInput } from '@query/api'
-import { useMutation } from '@tanstack/react-query'
-import type { NavProp } from '@utils/types'
-import React, { useEffect, useRef } from 'react'
-import { Image, TextInput, ToastAndroid, TouchableOpacity, View, type TextInputProps } from 'react-native'
+import {get_user_f, updateProfile_f, type UpdateProfileInput} from '@query/api'
+import {useMutation} from '@tanstack/react-query'
+import type {NavProp} from '@utils/types'
+import React, {useEffect, useRef} from 'react'
+import {Image, TextInput, ToastAndroid, TouchableOpacity, View, type TextInputProps} from 'react-native'
 
 export default function EditProfile({navigation}: NavProp) {
   const user = userStore((state) => state.user)
@@ -72,66 +73,68 @@ export default function EditProfile({navigation}: NavProp) {
         <BackHeader navigation={navigation} title='Profile' />
       </View>
       <Screen className='flex-1 px-5'>
-        <View className='flex-row items-center' style={{gap: 20}}>
-          <View className='flex-row'>
-            <Image
-              source={{
-                uri: 'https://i.pinimg.com/originals/1c/c5/35/1cc535901e32f18db87fa5e340a18aff.jpg',
+        <KeyboardAvoidingContainer>
+          <View className='flex-row items-center' style={{gap: 20}}>
+            <View className='flex-row'>
+              <Image
+                source={{
+                  uri: 'https://i.pinimg.com/originals/1c/c5/35/1cc535901e32f18db87fa5e340a18aff.jpg',
+                }}
+                className='h-20 w-20 rounded-full bg-white'
+              />
+            </View>
+            <View>
+              <SemiBold className='text-xl text-white'>Hi there {user?.data?.fname}</SemiBold>
+              <TouchableOpacity className='flex-row items-center pt-2' style={{gap: 8}} onPress={handleEditProfile}>
+                <PaintBrush01SolidIcon className='text-white/80' height={16} width={18} />
+                <SemiBold className='text-base text-white/80'>Edit Profile</SemiBold>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View className='mt-7' style={{gap: 15}}>
+            <InputComponent
+              ref={fnameInputRef}
+              label='First Name'
+              value={fname}
+              placeholder='Enter your first name'
+              editable={isEdit}
+              onChangeText={(text) => setFname(text)}
+            />
+            <InputComponent
+              label='Last Name'
+              value={lname}
+              placeholder='Enter your last name'
+              onChangeText={(text) => setLname(text)}
+              editable={isEdit}
+            />
+            <InputComponent
+              label='Email'
+              value={email}
+              keyboardType='email-address'
+              placeholder='Enter your email'
+              onChangeText={(text) => setEmail(text)}
+              editable={isEdit}
+            />
+            <InputComponent
+              label='Mobile'
+              value={user?.data?.mobileNumber}
+              placeholder='Enter your phone number'
+              editable={false}
+              keyboardType='phone-pad'
+              onPress={() => {
+                ToastAndroid.show('Mobile number cannot be changed', ToastAndroid.SHORT)
               }}
-              className='h-20 w-20 rounded-full bg-white'
             />
           </View>
-          <View>
-            <SemiBold className='text-xl text-white'>Hi there {user?.data?.fname}</SemiBold>
-            <TouchableOpacity className='flex-row items-center pt-2' style={{gap: 8}} onPress={handleEditProfile}>
-              <PaintBrush01SolidIcon className='text-white/80' height={16} width={18} />
-              <SemiBold className='text-base text-white/80'>Edit Profile</SemiBold>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View className='mt-7' style={{gap: 15}}>
-          <InputComponent
-            ref={fnameInputRef}
-            label='First Name'
-            value={fname}
-            placeholder='Enter your first name'
-            editable={isEdit}
-            onChangeText={(text) => setFname(text)}
-          />
-          <InputComponent
-            label='Last Name'
-            value={lname}
-            placeholder='Enter your last name'
-            onChangeText={(text) => setLname(text)}
-            editable={isEdit}
-          />
-          <InputComponent
-            label='Email'
-            value={email}
-            keyboardType='email-address'
-            placeholder='Enter your email'
-            onChangeText={(text) => setEmail(text)}
-            editable={isEdit}
-          />
-          <InputComponent
-            label='Mobile'
-            value={user?.data?.mobileNumber}
-            placeholder='Enter your phone number'
-            editable={false}
-            keyboardType='phone-pad'
-            onPress={() => {
-              ToastAndroid.show('Mobile number cannot be changed', ToastAndroid.SHORT)
-            }}
-          />
-        </View>
-        {isEdit &&
-          (isPending || userMutation.isPending ? (
-            <LoadingButton className='mt-10 rounded-2xl opacity-70' disabled />
-          ) : (
-            <FullGradientButton title='Save' className='mt-10' onPress={handleSave} />
-          ))}
-        <View className='h-10' />
-        <PaddingBottom />
+          {isEdit &&
+            (isPending || userMutation.isPending ? (
+              <LoadingButton className='mt-10 rounded-2xl opacity-70' disabled />
+            ) : (
+              <FullGradientButton title='Save' className='mt-10' onPress={handleSave} />
+            ))}
+          <View className='h-10' />
+          <PaddingBottom />
+        </KeyboardAvoidingContainer>
       </Screen>
     </View>
   )
